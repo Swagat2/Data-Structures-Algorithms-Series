030b3af92e43a01018ccbb85f366a15e23b28a00
Swagat2 <shreemanswagat2002@gmail.com>
2026-04-24 17:26:58 +0530

    new added

--- Sorting Algo/prac.cpp
+++ Sorting Algo/prac.cpp
@@ -1,5 +1,6 @@
 //Basic Bubble Sort (Foundation)
 //[5, 1, 4, 2, 8]
+#if 0
 #include<iostream>
 using namespace std;
 void bubblesort(int arr[],int n){
@@ -13,6 +14,31 @@ void bubblesort(int arr[],int n){
             }
         }
     }
+}
+int main(){
+    int arr[]={5, 1, 4, 2, 8};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    bubblesort(arr,n);
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    
+}
+#endif
+// Move Largest Element to End (Single Pass)
+// [5, 1, 4, 2, 8]
+// [1, 4, 2, 5, 8]  ans
+#include<iostream>
+using namespace std;
+void bubblesort(int arr[],int n){
+        for (int j = 0; j < n-1; j++)
+        {
+            if (arr[j]>arr[j+1])
+            {
+                swap(arr[j],arr[j+1]);
+            }
+        }
 }
 int main(){
     int arr[]={5, 1, 4, 2, 8};
